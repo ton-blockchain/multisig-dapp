f3df30ebab144d21a330d989c5dd046cbd31f107
Miandic <misha.andreev.novo@gmail.com>
2022-09-13 23:48:42 +0300

    Adding more publick key inputs by plus button

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+let id = 2
+
 const doSearch = () => {
     let address = $('.search-input').val()
     fetch('https://api.ton.cat/v2/explorer/getWalletInformation?address=' + address)
@@ -9,7 +11,16 @@ const doSearch = () => {
 }
 
 const goNew = () => {
+    window.location.href="new.html"
+}
 
+const addNew = () => {
+    id += 1
+    let pkph = 'Public key ' + id
+    let pkid = 'pubkey_' + id
+    $('.new-main-wrapper')[0].insertAdjacentHTML('beforeend', '<input type="text" class="new-input" value="">')
+    $('.new-input')[id-1].setAttribute('placeholder', pkph)
+    $('.new-input')[id-1].setAttribute('id', pkid)
 }
 
 $(window).on('load', () => {
@@ -17,12 +28,14 @@ $(window).on('load', () => {
     $('.search-input').on('keyup', e => {
         if (e.key === 'Enter' || e.keyCode === 13) doSearch()
     })
-    $('.search-button').click(doSearch);
-    $('#new').click(goNew);
+    $('#search_go').click(doSearch)
+    $('#search_new').click(goNew)
+
+    //for new
+    $('.new-add-button').click(addNew)
 
     //for wallet
     console.log(window.location)
     addr = window.location.href.split("?")
     $('.wallet-address').text(addr[1])
-
 })
